e5ab52c5e085907b57e3e0c128cf15ec6adbd0f7
AJSawatzky <andrassawatzky@gmail.com>
2022-10-23 22:32:48 -0700

    "Implement head assignment"

---
+++ head.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+};
+
+const head = function(array) {
+  let first = array[0];
+  console.log(first);
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
