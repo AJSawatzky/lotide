b4a087d513f564333a1f577fb3007126befc4165
AJSawatzky <andrassawatzky@gmail.com>
2022-10-30 22:06:39 -0700

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const eqArrays = (firstArray, secondArray) => {
+  if(firstArray.length !== secondArray){
+    return false;
+  }
+  for(let i = 0; i < firstArray.length; i++){
+    if(firstArray[i] !== secondArray[i]){
+      return false;
+    }
+  }return true;
+}
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+  if (eqArrays (actual === expected)) {
+    console.log(`✅✅✅ Assertion Passed:, ${actual}, '===', ${expected}`);
+    
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed:, ${actual}, '!==',  ${expected}`);
+  }
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
