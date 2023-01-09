b3cafd433848ce76d908b367a1ec34a095e4aa53
AJSawatzky <andrassawatzky@gmail.com>
2023-01-08 23:05:06 -0800

    refactor head, tail, and letterPositions

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = require('./assertEqual');
-
 const head = function(array) {
-  let first = array[0];
-  console.log(first);
+  return array[0];
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
--- letterPositions.js
+++ letterPositions.js
@@ -21,20 +21,22 @@ const assertArraysEqual = function(actual, expected) {
 
 
 const letterPositions = function(string) {
-  const results = {};
-    function getIndices(char, string){
-      var currentCharacterArray = [];
-      i = -1;
-        while ((i = string.indexOf(char, i + 1)) >= 0) {
-          currentCharacterArray.push(i);
-        }
-        return currentCharacterArray;
+  const answer = {};
+    
+  for (let i = 0; i < sentence.length; i++) {
+    let element = sentence[i];
+
+    if (element !== ' ') {
+      if (answer.hasOwnProperty(element)) {
+        answer[element].push(i);
+      } else {
+        answer[element] = [];
+        answer[element].push(i);
       }
-        for(const char of string){
-          results[char] = getIndices(char, string);
     }
-    return results;
+  }
+
+  return answer;
 };
 
-console.log(letterPositions('Lighthouse'));
-assertArraysEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
