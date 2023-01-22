bb749b8121e15bc0a86f14f7508a5e13503c2539
AJSawatzky <andrassawatzky@gmail.com>
2023-01-21 23:21:28 -0800

    refactor eqArrays and eqObjects

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,48 @@
 //takes in 2 arrays and returns true if the arrays are equal and false otherwise.
 
-const eqArrays = function(firstArray, secondArray)  {
-  for(let i = 0; i < firstArray.length; i++){
-    if(firstArray[i] !== secondArray[i]){
-      return false;
+const eqObjects = require("./eqObjects");
+
+//eqArray Function takes two arrays and checks that they are equal
+const eqArrays = function(actual, expected) {
+  let trueOrFalse = true;
+  // check to see that they are not underfined
+  if (actual === undefined || expected === undefined) {
+    trueOrFalse = false;
+  }
+  const returnTruth = function(actualA, expectedA) {
+  // check to make sure they are arrays and not objects
+    if ((Object.prototype.toString.call(actualA) !== "[object Array]") || (Object.prototype.toString.call(actualA) !== "[object Array]")) {
+      trueOrFalse = false;
     }
-  }
-  if (firstArray.length === secondArray.length) {
-    return true;
-  } else {
-    return false;
-  }
+
+    // check to see if their lengths are equal
+    if (actualA.length !== expectedA.length) {
+      trueOrFalse = false;
+    }
+
+  
+    // count through the given array arguments and if there is anything that doesn't match up - return false
+    for (let i in actualA) {
+
+      console.log(actualA[i], expectedA[i]);
+
+
+      // check to see that the index is an object
+      if (Object.prototype.toString.call(actualA[i]) === "[object Object]") {
+        if (!eqObjects(actualA[i], expectedA[i])) {
+          trueOrFalse = false;
+        }
+        i++;
+      }
+      if (Array.isArray(actualA[i])) {
+        returnTruth(actualA[i], expectedA[i]);
+      } else if (actualA[i] !== expectedA[i]) {
+        trueOrFalse = false;
+      }
+    }
+  };
+  returnTruth(actual, expected);
+  return trueOrFalse;
 };
 
 module.exports = eqArrays;
--- eqObjects.js
+++ eqObjects.js
@@ -1,30 +1,39 @@
 //takes in 2 objects and return true if they are equal and false otherwise.
 
-const eqArrays = require('./eqArrays')
-
-const eqObjects = function(obj1, obj2) {
-let keys1 = Object.keys(obj1)
-let keys2 = Object.keys(obj2)
-
-if (keys1.length === keys2.length) {
-  for (const element of keys1) {
-    if (obj1[element] instanceof Array && obj2[element] instanceof Array) {
-      if (!eqArrays(obj1[element], obj2[element])){
-        return false;
-      }
-    } else if(!(obj1[element] instanceof Array) && !(obj2[element] instanceof Array)) {
-      if (obj1[element] !== obj2[element]) {
-        return false;
-      }
-    } else {
-      return false;      
+const eqObjects = function(object1, object2) {
+  let check = true;
+  // Check for undefined
+  const compareObjects = function(object1, object2) {
+    if (object1 === undefined || object2 === undefined) {
+      check = false;
+      return check;
     }
-  }
-  return true;
-} else {
-  return false
-}
 
+    //compare the objects sizes
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+      check = false;
+    }
+  
+    // count through both objects and compare key-value pairs
+    for (let key of Object.keys(object1)) {
+    // if the value is an array
+      /*if (Array.isArray(object1[key])) { //TODO FOR LATER CHECK FOR ARRAYS CURRENTLY CIRCULAR DEPENDANCY
+      // if the arrays are different
+        if (!eqArrays(object1[key], object2[key])) {
+          check = false;
+        }
+      } else */
+      
+      if (Object.prototype.toString.call(object1[key]) === "[object Object]") {
+        compareObjects(object1[key], object2[key]);
+
+      } else if (object1[key] !== object2[key]) {
+        check = false;
+      }
+    }
+  };
+  compareObjects(object1, object2);
+  return check;
 };
 
-module.exports = eqObjects
+module.exports = eqObjects;
